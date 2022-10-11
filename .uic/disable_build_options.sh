eaeff217c1a1ff8cedecc3696f31c63647546dd3
Sven Rademakers <sven.rademakers@gmail.com>
2022-10-11 08:53:56 +0200

    uic commit

---
+++ .uic/disable_build_options.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+regex='option\((BUILD_(:?[A-Z]|_)+)'
+input="$UIC_ROOT/cmake/include/build_permutations.cmake"
+while read -r line
+do
+    if [[ $line =~ $regex ]] && [[ ${BASH_REMATCH[1]} != "$1" ]]
+    then
+       printf "%s " "-D${BASH_REMATCH[1]}=OFF"
+    fi
+done < $input
