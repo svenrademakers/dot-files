3ac19e3c0f6f46ad1cacbd9c6e5bcad881fe26bc
Sven Rademakers <sven.rademakers@gmail.com>
2023-08-02 08:35:02 +0100

    hotplug_monitor.sh: external monitor udev script

---
+++ .config/hotplug_monitor.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+	
+export DISPLAY=:0
+external_display_status=`cat /sys/class/drm/card1-DP-1/status`
+
+send_notification()
+{
+    notify-send Display "External monitor $external_display_status"
+}
+
+connect ()
+{
+   xrandr --output eDP-1-1 --off
+   xrandr --output DP-0 --auto
+   xrandr --setprovideroutputsource modesetting NVIDIA-0
+   nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+}
+
+disconnect ()
+{
+   xrandr --output eDP-1-1 --auto
+}
+
+[[ "$external_display_status" == "connected" ]] && connect || disconnect
+send_notification
