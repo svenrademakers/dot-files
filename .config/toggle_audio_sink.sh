ed3e2ab011e275c5dc756a941d0c7b56250ef76d
Sven Rademakers <sven.rademakers@gmail.com>
2024-10-04 11:42:46 +0100

    i3 styling update
    updated i3status bar
    added picom again
    cleaned up i3 config

--- .config/toggle_audio_sink.sh
+++ .config/toggle_audio_sink.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e
+
 if [[ $(pactl info) =~ Default\ Sink:\ .*\Hyper.* ]]; then
     pactl set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo
+    notify-send "Audio" "HDMI output selected"
 else
     pactl set-default-sink alsa_output.usb-Kingston_HyperX_Cloud_II_Wireless_000000000001-00.analog-stereo
+    notify-send "Audio" "Headphones output selected"
 fi
