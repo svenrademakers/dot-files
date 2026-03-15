dda37afbc4097780891b888b3a30b0891b8f85db
Sven Rademakers <sven.rademakers@gmail.com>
2026-03-15 13:16:21 +0000

    remove nvim remaps, sway updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,25 +26,6 @@ vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
 vim.opt.wrap = true
 vim.g.mapleader = " "
-vim.keymap.set('n', 'n', 'j', { silent = true }) -- down
-vim.keymap.set('n', 'e', 'k', { silent = true }) -- up
-vim.keymap.set('n', 'i', 'l', { silent = true }) -- right
-vim.keymap.set('n', 'j', 'e', { silent = true }) -- end of word
-vim.keymap.set('n', 'J', 'E', { silent = true }) -- end of word
-vim.keymap.set('n', 'k', 'n', { silent = true }) -- next search
-vim.keymap.set('n', 'K', 'N', { silent = true }) -- next search
-vim.keymap.set('n', 'l', 'i', { silent = true }) -- insert
-vim.keymap.set('n', 'L', 'I', { silent = true }) -- insert
-
----- extra remap
-vim.keymap.set('n', '<leader>e', 'e', { silent = true })
-vim.keymap.set('n', '<leader>i', 'i', { silent = true })
-vim.keymap.set('n', '<leader>n', 'n', { silent = true })
--- Make `im` behave like `iw`
-vim.keymap.set('o', 'im', 'iw')
-vim.keymap.set('x', 'im', 'iw')  -- also for visual mode (viw → vim)
-vim.keymap.set('n', 'l', 'c')
-vim.keymap.set('o', 'l', 'c')
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     pattern = {"*"},
